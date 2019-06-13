a9ad294d9f5b34a75139a1d4dad0aea92bc84f0e
Devin Alvaro <devin.alvaro@gmail.com>
2019-06-13 20:53:48 +0700

    Parse user_id in bin/client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,18 @@
+use std::env;
+
 use point::client::Client;
 
 fn main() {
     // TODO: parse flags
-    let client = Client::new(1, "127.0.0.1", "7878");
+    let mut args = env::args();
+    args.next();
+
+    let user_id = if let Some(user_id) = args.next() {
+        user_id.trim().parse::<i64>().unwrap()
+    } else {
+        1
+    };
+
+    let client = Client::new(user_id, "127.0.0.1", "7878");
     client.start();
 }
